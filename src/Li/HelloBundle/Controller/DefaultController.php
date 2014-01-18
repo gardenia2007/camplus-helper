5fc5c5fc48b0973b68667155c310a8e9a5f18a4c
李洪祥 <gardeniaxy@gmail.com>
2014-01-18 16:43:06 +0800

    simple

--- src/Li/HelloBundle/Controller/DefaultController.php
+++ src/Li/HelloBundle/Controller/DefaultController.php
@@ -9,48 +9,51 @@ use Symfony\Component\HttpFoundation\Response;
 
 define('TOKEN', 'hahahaha');
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
+class DefaultController extends Controller {
+
+    public function indexAction($name) {
         return $this->render('LiHelloBundle:Default:index.html.twig', array('name' => $name));
     }
 
     public function testAction(){
-    	$xml = ' <xml>
-			 <ToUserName><![CDATA[toUser]]></ToUserName>
-			 <FromUserName><![CDATA[fromUser]]></FromUserName> 
-			 <CreateTime>1348831860</CreateTime>
-			 <MsgType><![CDATA[text]]></MsgType>
-			 <Content><![CDATA[this is a test]]></Content>
-			 <MsgId>1234567890123456</MsgId>
-		 </xml>';
-	$s = new Serializer();
-	$d = $s->decode($xml, 'xml');
-	var_dump($d);
-	exit();
     	$data = array(
     		'toUser' => '12312',
     		'fromUser' => '123123',
     		'time' => time(),
     		'type' => 'text',
-    		'content' => 'hello',
+    		'content' => 'lalala',
     		);
     	return $this->render('LiHelloBundle:Default:text.xml.twig', $data);
     }
 
     public function weixinAction(Request $request){
+	// 微信一般接口
+    	if($request->server->get('REQUEST_METHOD') == 'POST'){
+	    	$post_data = file_get_contents('php://input'); // 微信服务器POST的xml数据
+		$d = simplexml_load_string($post_data, 'SimpleXMLElement', LIBXML_NOCDATA);
+	    	$data = array(
+	    		'toUser' => $d->fromUser,
+	    		'fromUser' => $d->toUser,
+	    		'time' => time(),
+	    		'type' => 'text',
+	    		'content' => $d->Content,
+    		);
+	    	return $this->render('LiHelloBundle:Default:text.xml.twig', $data);
+    	}else{ // 微信验证
+		if ($this->checkSignature($request)) {
+			echo $request->query->get('echostr');
+		}else{
+			echo 'opps...';
+		}
+		exit();
+    	}
+    }
+
+    private function checkSignature($request) {
+	$token = TOKEN;
     	$signature = $request->query->get('signature');
     	$timestamp = $request->query->get('timestamp');
     	$nonce = $request->query->get('nonce');
-    	$echostr = $request->query->get('echostr');
-	$token = TOKEN;
-	if ($this->checkSignature($token, $signature, $timestamp, $nonce)) {
-		echo $echostr;
-	}
-    }
-
-    private function checkSignature($token, $signature, $timestamp, $nonce) {
 	$tmpArr = array($token, $timestamp, $nonce);
 	sort($tmpArr);
 	$tmpStr = implode( $tmpArr );
